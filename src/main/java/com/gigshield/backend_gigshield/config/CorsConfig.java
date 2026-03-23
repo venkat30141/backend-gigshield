75a479abbb1f012af50e9c683700be07df250622
venkat <dupaguntlashiva999@gmail.com>
2026-03-23 17:49:03 +0530

    Update CORS configuration to allow all origins

--- src/main/java/com/gigshield/backend_gigshield/config/CorsConfig.java
+++ src/main/java/com/gigshield/backend_gigshield/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
